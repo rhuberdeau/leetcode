9195833526e2a13886516e4120beca107113e1a0
rhuberdeau <rhuberdeau@gmail.com>
2016-11-06 11:40:03 -0500

    adding one pass solution

--- two_sum/two_sum.rb
+++ two_sum/two_sum.rb
@@ -1,12 +1,21 @@
 class TwoSum
+  attr_reader :array, :target, :hash
+
+  def initialize(array, target)
+    @array  = array
+    @target = target
+  end
+
   # param {Array[]}, numbers
   # param {Integer}, target number
   # return {Array} of zero based indices
-  def self.two_sum(nums, target)
-    nums.each do |val|
-      array_with_val_removed = nums - [val]
-      array_with_val_removed.each do |other_val|
-        return [nums.index(val), nums.index(other_val)] if val + other_val == target
+  def run
+    hash = {}
+    array.each_with_index do |value, index|
+      if hash.has_value?(target-value)
+        return [index, hash.key(target-value)].sort
+      else
+        hash[index] = value
       end
     end
   end
